0830ea5ce8bfad128f2633e8273f3fca21654061
jjohnson673 <jkjohnson673@gmail.com>
2022-12-29 14:23:15 -0500

    added in npm and sarted index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var express = require('express')
+var ejs = require('ejs')
+
+var app = express();
+
+
+//tells express to use the pubic folder to deliver the css/images/js in our application
+app.use(express.static('public'));
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.listen(3006);
+
+//local host 3006
+app.get('/',function(req,res){
+
+    //don't need to put Views folder, node already sees 'Views', just put folder name within Views
+    //and then name of the file needed (no .ejs needed)
+res.render('pages/index');
+
+
+});
