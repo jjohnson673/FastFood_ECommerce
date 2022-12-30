055ac24e83623659c87e34e1fcf8c70da2275b1e
jjohnson673 <jkjohnson673@gmail.com>
2022-12-29 19:43:36 -0500

    added in express-sessions and ran into issues with forEach in index.ejs

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 var express = require('express')
 var ejs = require('ejs')
 var bodyParser = require('body-parser');
+var mysql = require('mysql')
+var session = require('express-session');
+
+mysql.createConnection({
+
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"node_foodproject"
+
+})
 
 
 var app = express();
@@ -13,15 +24,29 @@ app.use(express.static('public'));
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.listen(3006);
+app.listen(3007);
 app.use(bodyParser.urlencoded({extended:true}));
 
-//local host 3006
+
+//local host 3007
 app.get('/',function(req,res){
 
+
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"node_foodproject"
+})
+
+
+con.query("SELECT * FROM products",(err,result)=>{
     //don't need to put Views folder, node already sees 'Views', just put folder name within Views
     //and then name of the file needed (no .ejs needed)
-res.render('pages/index');
+    res.render('pages/index',{result:result});
+})
+
+
 
 
 });
